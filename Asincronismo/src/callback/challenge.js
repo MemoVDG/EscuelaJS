5092f976eb23344edabf7cfe85a722f36e6dce94
MemoVDG <memovdg@gmail.com>
2020-03-10 12:44:31 -0600

    Asincronismo

--- Asincronismo/src/callback/challenge.js
+++ Asincronismo/src/callback/challenge.js
@@ -1,8 +1,11 @@
-let xmlhttprequest = require('xmlhttprequest').XMLHttpRequest;
+let XMLHttpRequest = require('xmlhttprequest').XMLHttpRequest;
+let API = 'https://rickandmortyapi.com/api/character/';
 
 function fetchData(url_api, callback) {
     let xhttp = new XMLHttpRequest
-    // Tipo de request, URL, Si se maneja de manera asincrona
+    /*  Abrimos la conexion 
+        Tipo de request, URL, Si se maneja de manera asincrona
+    */
     xhttp.open('GET', url_api, true)
 
     xhttp.onreadystatechange = function (event) {
@@ -26,3 +29,28 @@ function fetchData(url_api, callback) {
     }
     xhttp.send();
 }
+
+fetchData(API, function (error1, data1) {
+    if (error1) {
+        return console.log(error1);
+    } else {
+        /*  Anidamos los llamados a la API, solo cambiamos la URL de destino
+            concatenando valores
+        */
+        fetchData(API + data1.results[0].id, function (error2, data2) {
+            if (error2) {
+                return console.log(error2);
+            } else {
+                fetchData(data2.origin.url, function (error3, data3) {
+                    if (error3) {
+                        return console.log(error3);
+                    } else {
+                        console.log(data1.info.count);
+                        console.log(data2.name);
+                        console.log(data3.dimension);
+                    }
+                })
+            }
+        });
+    }
+});
