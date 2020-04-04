6636d6738b766a9c3c95e96e37a1012c225e25f6
Memo <memovdg@gmail.com>
2020-04-04 13:00:52 -0600

    Display ads

--- CursoProfesionalJS/assets/index.ts
+++ CursoProfesionalJS/assets/index.ts
@@ -1,14 +1,17 @@
 const video = document.querySelector("video");
 const button: HTMLElement = document.querySelector("button");
-const muteVideo:HTMLElement = document.getElementById('mute');
+const muteVideo: HTMLElement = document.getElementById('mute');
 import MediaPlayer from './MediaPlayer';
 import AutoPlay from './plugins/AutoPlay';
 import AutoPause from './plugins/AutoPause';
+import Ads from './plugins/Ads';
 
-const player = new MediaPlayer({ el: video, plugins: [
+const player = new MediaPlayer({
+    el: video, plugins: [
         new AutoPlay(),
-        new AutoPause()
-    ] 
+        new AutoPause(),
+        new Ads()
+    ]
 });
 
 
@@ -27,7 +30,7 @@ muteVideo.onclick = () => {
 /*  Los service workers nos van a permitir guardar informacion cuando el usuario se encuentra offline
 */
 // Verificamos si el navegador soporta serviceworkers
-if('serviceWorker' in navigator) {
+if ('serviceWorker' in navigator) {
     // Registramos el archivo donde se encuentra el service worker
     navigator.serviceWorker.register('/sw.js').catch(error => {
         console.log(error.message);
