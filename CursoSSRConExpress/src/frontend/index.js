4de8d9b6e944dc0ec112a90ae73e93052c8aec75
Memo Vara <memovdg@gmail.com>
2020-07-11 13:43:29 -0500

    [ADD] SSR configuracion de assets, eventos y enviar paginas en texto plano

--- CursoSSRConExpress/src/frontend/index.js
+++ CursoSSRConExpress/src/frontend/index.js
@@ -10,21 +10,28 @@ import { createStore, compose } from 'redux';
 import reducer from './reducers';
 
 // SSR
-import {createBrowserHistory} from 'history';
-import {Router} from 'react-router';
-import initialState from './intialState';
-
+import { createBrowserHistory } from 'history';
+import { Router } from 'react-router';
+//import initialState from './intialState';
 
 const history = createBrowserHistory();
+
+const preloadeState = window.__PRELOADED_STATE__;
 
 //Variable para conectar la extension de REDUX con la app y poder hacer debug
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 /* Creacion del store que recibe un reducer que viene de la carpeta reducer/index.js
  y el estado inicial de la app  */
-const store = createStore(reducer, initialState, composeEnhancers());
+const store = createStore(reducer, preloadeState, composeEnhancers());
 
-ReactDOM.render(
+// Lo cargamos y una vez que se ocupa se elimina para que
+// los usuarios desde consola de JS no puedan modificar el estado
+delete window.__PRELOADED_STATE__;
+
+// Hydrate nos sirve cuando hacemos SSR
+// y con el se esuchan los eventos de la aplicacion
+ReactDOM.hydrate(
 	/* Proporcionamos el estado inicial al provider y el reducer que hara los cambios
     y regresara los cambios
   */
--- CursoSSRConExpress/src/server/server.js
+++ CursoSSRConExpress/src/server/server.js
@@ -1,35 +1,35 @@
 import express from 'express';
 import dotenv from 'dotenv';
-import webpack from 'webpack'
+import webpack from 'webpack';
 import React from 'react';
-import ReactDom from 'react-dom';
-import { Provider} from 'react-redux';
-import { createStore, compose} from 'redux';
-import {StaticRouter} from 'react-router-dom'
+import { renderToString } from 'react-dom/server';
+import { Provider } from 'react-redux';
+import { createStore, compose } from 'redux';
+import { StaticRouter } from 'react-router-dom';
 import reducer from '../frontend/reducers';
 import intialState from '../frontend/intialState';
+import { renderRoutes } from 'react-router-config';
+import serverRoutes from '../frontend/routes/serverRoutes';
 
 dotenv.config();
-const {ENV, PORT} = process.env;
 
+const { ENV, PORT } = process.env;
 const app = express();
 
-if(ENV === 'development'){
-    console.log('Development config');
-    const webpackConfig = require('../../webpack.config');
-    const webpackDevMiddleware = require('webpack-dev-middleware');
-    const webpackHotMiddleware = require('webpack-hot-middleware');
-    const compiler = webpack(webpackConfig);
-    const serverConfig = {
-        port: PORT,
-        hot: true,
-
-    };
+if (ENV === 'development') {
+	console.log('Development config');
+	const webpackConfig = require('../../webpack.config');
+	const webpackDevMiddleware = require('webpack-dev-middleware');
+	const webpackHotMiddleware = require('webpack-hot-middleware');
+	const compiler = webpack(webpackConfig);
+	const serverConfig = {
+		port: PORT,
+		hot: true,
+	};
 }
 
-app.get('*', (req, res) => {
-    res.send(
-        `
+const setResponse = (html, preloadedState) => {
+	return `
     <!DOCTYPE html>
     <html lang="en">
         <head>
@@ -39,19 +39,37 @@ app.get('*', (req, res) => {
             <title>VdO</title>
         </head>
         <body>
-            <div id="app"></div>
+						<div id="app">${html}</div>
+						<script>
+          			window.__PRELOADED_STATE__ = ${JSON.stringify(preloadedState).replace(/</g, '\\u003c')}
+        		</script>``
             <script src="assets/app.js" type="text/javascript">
             </script>
         </body>
     </html>
-    `
-    );
-});
+    `;
+};
+
+const renderApp = (req, res) => {
+	const store = createStore(reducer, intialState);
+	const preloadedState = store.getState();
+	const html = renderToString(
+		<Provider store={store}>
+			<StaticRouter location={req.url} context={{}}>
+				{renderRoutes(serverRoutes)}
+			</StaticRouter>
+		</Provider>
+	);
+
+	res.send(setResponse(html, preloadedState));
+};
+
+app.get('*', renderApp);
 
 app.listen(PORT, (err) => {
-    if (err){
-        console.log(err)
-    }else {
-        console.log('Listen on port 3000')
-    }
+	if (err) {
+		console.log(err);
+	} else {
+		console.log('Listen on port 3000');
+	}
 });
